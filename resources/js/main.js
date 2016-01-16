f53d4dbb77b7aae4ef00d77ac1d9c00b472b8244
lukss12 <picchi.lucas@gmail.com>
2016-01-16 10:49:35 -0300

    Fix triangle drawing

--- resources/js/main.js
+++ resources/js/main.js
@@ -18,7 +18,7 @@ Drawer.prototype.drawTriangle = function(x1, y1, x2, y2, x3, y3) {
     context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
-    context.lineTo(y3, y3);
+    context.lineTo(x3, y3);
     context.closePath();
 
     // the outline
